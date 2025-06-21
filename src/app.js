11e5fa4ba6b24c5b0a9ffd8195d5ad8f357d2504
AnthonyTiwari <Anthonyptiwari90@gmail.com>
2025-06-21 15:48:43 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById('domainname').innerHTML = GenerateDomainName(pronoun, adj, noun, extension).join("");
+}
+
+function GenerateDomainName(pronoun, adj, noun, extension) {
+  let results = [];
+  let count = 1;
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extension) {
+          results.push(count + ". " + p + a + n + e + "<br>");
+          count++;
+        }
+      }
+    }
+  }
+
+  return results;
+}
+
+let pronoun = ['the', 'our',];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.us', '.io', '.dev', '.org',];
